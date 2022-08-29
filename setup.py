02ed79f78d0c070333846b9c459fa882a848cdb3
couto0 <daniel.couto64@gmail.com>
2022-08-29 13:34:07 -0300

    release 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="altprint",
-    version="0.1.2",
+    version="0.2.0",
     author="couto0",
     author_email="daniel.couto64@gmail.com",
     description="Alternative 3d printing",
@@ -20,5 +20,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=['numpy>=1.19.4', 'Shapely>=1.7.1', 'trimesh>=3.9.1', 'PuLP>=2.4', 'scipy>=1.7.0', 'networkx>=2.5.1', 'rtree>=0.9.7'],
+    install_requires=['numpy>=1.19.4', 'Shapely>=1.7.1', 'trimesh>=3.9.1', 'PuLP>=2.4', 'scipy>=1.7.0', 'networkx>=2.5.1', 'rtree>=0.9.7', 'PyYAML>=6.0'],
 )
