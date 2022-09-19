6422cd42fc5fd677ba3f49326dc01ec932a71dc7
couto0 <daniel.couto64@gmail.com>
2022-09-19 11:07:59 -0300

    fixed connection points issues

--- rectilinear_infill.py
+++ rectilinear_infill.py
@@ -8,7 +8,7 @@ border = [(0, 0), (10, 0), (10, 10), (20, 10), (20, 5), (30, 5), (30, 40), (10,2
 holes = [[(5, 15), (15, 15), (15, 20), (5, 20)], [(20, 25), (25, 25), (25, 30)]]
 shape = Polygon(border, holes)
 
-gap = 0.5
+gap = 3
 thres = 0
 
 # y = m*x+c
@@ -20,7 +20,7 @@ def x_from_y(a, b, yc):
     dx = b[0]-a[0]
     dy = b[1]-a[1]
     if dx==0:
-        return a[0]
+        return np.ones(len(yc))*a[0]
     return (yc - a[1])*dx/dy + a[0]
 
 
@@ -30,22 +30,13 @@ for i in range(len(coords)-1):
     a = coords[i]
     b = coords[i+1]
     dy = (b[1] - a[1])
-    if dy > thres:
-        points.append(a)
-        ys = np.arange(int(a[1]/gap)+1, int(b[1]/gap)+1)*gap
-        xi = x_from_y(a, b, ys[0])
-        xf = x_from_y(a, b, ys[-1])
-        xs = np.linspace(xi, xf, len(ys))
-        points.extend(list(zip(xs, ys)))
-
-    if dy < -thres:
-        points.append(a)
-        ys = np.arange(int(a[1]/gap), int(b[1]/gap), -1)*gap
-        xi = x_from_y(a, b, ys[0])
-        xf = x_from_y(a, b, ys[-1])
-        xs = np.linspace(xi, xf, len(ys))
-        points.extend(list(zip(xs, ys)))
-points.append(coords[-1])
+    ys = np.array([])
+    if dy > thres: #b > a
+        ys = np.arange(np.ceil(a[1]/gap), np.floor(b[1]/gap)+1)*gap
+    if dy < -thres: #b < a
+        ys = np.arange(np.floor(a[1]/gap), np.ceil(b[1]/gap-1), -1)*gap
+    xs = x_from_y(a, b, ys)
+    points.extend(list(zip(xs, ys)))
 
 mp = MultiPoint(points)
 
