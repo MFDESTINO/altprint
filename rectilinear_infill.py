ff96b9914565005fde4ab4d8cb7b5286bccf07ca
couto0 <daniel.couto64@gmail.com>
2022-09-23 19:49:18 -0300

    matrix sorting evil bit hack

--- rectilinear_infill.py
+++ rectilinear_infill.py
@@ -1,5 +1,6 @@
 from shapely.geometry import LineString, Polygon, MultiPoint
 import numpy as np
+from collections import deque
 
 def x_from_y(a, b, yc):
     dx = b[0]-a[0]
@@ -8,66 +9,63 @@ def x_from_y(a, b, yc):
         return np.ones(len(yc))*a[0]
     return (yc - a[1])*dx/dy + a[0]
 
-def get_connection_lines(shape):
-    coords = shape.coords
-    connection_lines = []
-    for i in range(len(coords)-1):
-        a = coords[i]
-        b = coords[i+1]
-        dy = (b[1] - a[1])
-        ys = np.array([])
-        if dy > thres: #b > a
-            ys = np.arange(np.ceil(a[1]/gap), np.floor(b[1]/gap)+1)*gap
-        if dy < -thres: #b < a
-            ys = np.arange(np.floor(a[1]/gap), np.ceil(b[1]/gap-1), -1)*gap
-        xs = x_from_y(a, b, ys)
-        points = list(zip(xs, ys))
-        for i in range(len(points)-1):
-            connection_lines.append(LineString([points[i], points[i+1]]))
-    return connection_lines
+def get_column(a, b, gap, height):
+    maxy = max(a[1], b[1])
+    miny = min(a[1], b[1])
+    start = int(np.ceil(miny/gap))
+    end = int(np.floor(maxy/gap)+1)
+    ys = np.arange(start, end)*gap
+    xs = x_from_y(a, b, ys)
+    mask = np.ones(height)
+    mask[start:end] = 0
+    col = np.ma.array(np.zeros(height), mask=mask)
+    col[start:end] = xs
+    return col
 
-def make_x_matrix(shape, gap):
+def get_cols(shape, gap, thres):
     coords = shape.coords
     x, y = shape.xy
-    max_y = shape.bounds[3]
-    xv = np.unique(x)
-    xm = np.ones([len(xv), int(max_y, gap)]) * -1
-    
+    height = int(np.floor(shape.bounds[3]/gap)+1)
+    cols = []
     for i in range(len(coords)-1):
         a = coords[i]
         b = coords[i+1]
-        dy = (b[1] - a[1])
-        ys = np.array([])
-        if dy > thres: #b > a
-            ys = np.arange(np.ceil(a[1]/gap), np.floor(b[1]/gap)+1)*gap
-        if dy < -thres: #b < a
-            ys = np.arange(np.ceil(b[1]/gap), np.floor(a[1]/gap)+1)*gap
-        xs = x_from_y(a, b, ys)
-        points = list(zip(xs, ys))
+        dx = b[0] - a[0]
+        dy = b[1] - a[1]
+        if abs(dy) > thres:
+            col = get_column(a, b, gap, height)    
+            cols.append(col)
+    return np.ma.array(cols)
+
+def sort_cols(cols):
+    for i in range(cols.shape[1]):
+        col = cols[:,i]
+        valid_indexes = (col+1).nonzero()[0]
+        sorted_indexes = valid_indexes[col[valid_indexes].argsort()]
+        cols[[valid_indexes]] = cols[[sorted_indexes]]
+ 
+
+def print_matrix(m):
+    rowf = "{:3.2f} "* m.shape[1]
+    for row in m:
+        print(rowf.format(*row))
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
-
+    np.set_printoptions(precision=3)
     #shape = Polygon([(0,0), (10,0), (10, 10), (0, 10)])
     border = [(0, 0), (10, 0), (10, 10), (20, 10), (20, 5), (30, 5), (30, 40), (10,25), (0, 30), (3, 15)]
     holes = [[(5, 15), (15, 15), (15, 20), (5, 20)], [(20, 25), (25, 25), (25, 30)]]
     poly = Polygon(border, holes)
 
-    gap = 2
+    gap = 5
     thres = 0
-    connection_lines = get_connection_lines(poly.exterior)
-    #for hole in poly.interiors:
-    #   connection_lines.extend(get_connection_lines(hole))
+    cols = get_cols(poly.exterior, gap, thres)
+    sort_cols(cols)
+    print_matrix(cols)
     fig, ax = plt.subplots()
-
     x, y = poly.exterior.xy
     ax.plot(x, y, color="black", linewidth=3)
-    #for hole in poly.interiors:
-    #   x, y = hole.xy
-    #  ax.plot(x, y, color="black", linewidth=3)
-        
-    for line in connection_lines:
-        x, y = line.xy
-        ax.plot(x, y, linewidth=5)
+
     ax.axis("equal")
     plt.show()
